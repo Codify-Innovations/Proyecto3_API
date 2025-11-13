5acbade065418710dc347a00947fe5e2aaaad0c6
Gabriel Bryan <gbryans@ucenfotec.ac.cr>
2025-11-12 19:58:59 -0600

    Update Analisis y puntuasion de contenido

--- AutoCut IA/app/api/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import predict
-
-api_router = APIRouter()
-
-# Prefijos y tags por módulo
-api_router.include_router(predict.router, prefix="/api/predict", tags=["Predicción"])
--- AutoCut IA/app/main.py
+++ AutoCut IA/app/main.py
@@ -1,23 +1,14 @@
 from fastapi import FastAPI
-from app.api.endpoints import predict, analyze
-from app.core.config import settings
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.endpoints.analyze import router as analyze_router
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
-
+app = FastAPI(title="AutoCut Multimedia Quality AI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(predict.router, prefix="/api/predict", tags=["Predicción"])
-app.include_router(analyze.router, prefix="/api", tags=["Análisis Multimedia"])
-
-
-@app.get("/")
-def root():
-    return {"mensaje": "API funcionando correctamente"}
+app.include_router(analyze_router, prefix="/api", tags=["Media Analysis"])
